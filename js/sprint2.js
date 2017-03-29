afa489e5057078f39d8e4f1767edb92444055412
YurianKB <yuriankb@gmail.com>
2017-03-29 11:09:32 +0000

    Ya aparece el texto ingresado (quiero llorar :') ) falta alistarlo y terminar TODO ajaja

---
+++ js/sprint2.js
@@ -0,0 +1,12 @@
+
+
+function agregarPendiente (){
+    var nuevoElemento = document.getElementById("texto").value;
+    var elementoLista = document.createElement("li");
+    espacioTexto.innerHTML = nuevoElemento;
+    console.log(agregarPendiente);
+    agregarPendiente.appendChild(li);
+};
+
+/*var nuevoElemento = document.getElementById("texto");
+var elementos = document.getElementById("")*/
