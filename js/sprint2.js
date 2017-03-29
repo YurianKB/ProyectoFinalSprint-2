7837f40b547511309d1f75b6022d71cce3067502
YurianKB <yuriankb@gmail.com>
2017-03-29 13:25:26 +0000

    Agregué funciones para las listas sin resultados...AÚN :c

--- js/sprint2.js
+++ js/sprint2.js
@@ -1,12 +1,34 @@
-
-
 function agregarPendiente (){
     var nuevoElemento = document.getElementById("texto").value;
     var elementoLista = document.createElement("li");
     espacioTexto.innerHTML = nuevoElemento;
-    console.log(agregarPendiente);
-    agregarPendiente.appendChild(li);
-};
 
-/*var nuevoElemento = document.getElementById("texto");
-var elementos = document.getElementById("")*/
+    agregarPendiente.appendChild("texto");
+ };
+
+
+
+/*function agregarPendiente (){
+    var nuevoElemento = document.getElementById("texto").value;
+    var elementoLista = document.createElement("li");
+
+    espacioTexto.innetHTML = nuevoElemento;
+
+    agregarPendiente.appendChild(li);
+};*/
+
+
+function agregarPendiente2 (){
+    var nuevoElemento2 = document.getElementById("texto2").value;
+    var elementoLista2 = document.createElement("li");
+    espacioTexto2.innerHTML = nuevoElemento2;
+
+    agregarPendiente2.appendChild("li");
+}
+
+
+function crearLista (){
+  var nombreLista = document.getElementById("agregarLista").value;
+  espacioNuevaLista.innerHTML = nombreLista;
+
+}
